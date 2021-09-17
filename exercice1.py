ffbf1ad1e5b37216a6a599bb6bd65b559ad86742
Sosa013 <89996378+Sosa013@users.noreply.github.com>
2021-09-17 06:31:28 -0700

    changement 4

--- exercice1.py
+++ exercice1.py
@@ -3,20 +3,22 @@ def fizzBuzz(n):
     #  Assigner ensuite la valeur à la variable resultat
 # Exercice 1
 
-    if n % 3 == 0 :
-        print("fizz est un multiple de 3")
-
-
-    if n % 5 == 0 :
-        print("buzz est un multiple de 5")
-
-
     if (n % 3 == 0) and (n % 5 == 0):
-        print("fizzbuzz est à la fois un multiple de 3 et 5")
+        resultat = "FizzBuzz"
+        return resultat
+
+    elif n % 3 == 0 :
+        resultat = "Fizz"
+        return resultat
 
 
-    resultat = n
-    return resultat
+    elif n % 5 == 0 :
+        resultat = "Buzz"
+        return resultat
+
+    else:
+        resultat = n
+        return resultat
 
 if __name__ == '__main__':
     n = int(input("indiquez le nombre: "))
