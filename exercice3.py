8244fb7f8b93e5737473d8a8b19c294b7716d2c4
Sosa013 <89996378+Sosa013@users.noreply.github.com>
2021-09-21 12:23:12 -0700

    Version final

--- exercice3.py
+++ exercice3.py
@@ -2,28 +2,28 @@
 def decomposer(secondes):
 
     # TODO: Assigner à la variable "annees" le nombre d'années
-    annees = secondes // (60*60*24*365)
-    secondes_restantes01 = secondes - (annees * (60*60*24*365))
+    annees = int(secondes / (60*60*24*365))
+    secondes_restantes1 = secondes - (annees * (60*60*24*365))
 
     # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-    semaines = secondes_restantes01 // (60*60*24*7)
-    secondes_restantes02 = secondes_restantes01 - (semaines * (60*60*24*7))
+    semaines = int(secondes_restantes1 / (60*60*24*7))
+    secondes_restantes2 = secondes_restantes1 - (semaines * (60*60*24*7))
 
     # TODO: Assigner à la variable "jours" le nombre de jours restants
-    jours = secondes_restantes02 // (60*60*24)
-    secondes_restantes03 = secondes_restantes02 - (jours * (60*60*24))
+    jours = int(secondes_restantes2 / (60*60*24))
+    secondes_restantes3 = secondes_restantes2 - (jours * (60*60*24))
 
     # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-    heures = secondes_restantes03 // (60*60)
-    secondes_restantes04 = secondes_restantes03 - (heures * (60*60))
+    heures = int(secondes_restantes3 / (60*60))
+    secondes_restantes4 = secondes_restantes3 - (heures * (60*60))
 
     # TODO: Assigner à la variable "minute" le nombre de minutes restantes
-    minutes = secondes_restantes04 // (60)
-    secondes_restantes05 = secondes_restantes04 - (minutes * (60))
+    minutes = int(secondes_restantes4 / (60))
+    secondes_restantes5 = secondes_restantes4 - (minutes * (60))
 
     # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
-    secondes = secondes_restantes05
-    secondes_restantes5 = secondes_restantes05
+    secondes = secondes_restantes5
+    secondes_restantes6 = secondes_restantes5
 
     # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
 
